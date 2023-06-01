15bfd3ad5b33890a8151cdac63f9fc5be6133f23
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-05-31 23:27:51 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   }, [])
   return (
     <main>
-      <h1 className="font-bold text-2xl">Plagiarism Fronst</h1>
+      <h1 className="font-bold text-2xl">Plagiarism Front</h1>
       <h2 className="font-bold text-2xl">
         {process.env.NEXT_PUBLIC_PLAGIARISM_API}
       </h2>
