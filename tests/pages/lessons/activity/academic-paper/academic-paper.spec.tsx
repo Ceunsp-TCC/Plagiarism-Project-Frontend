80e9a7ff908de4e2af35783084c2a70d0276d72c
Sairan Serra <sairan.serra@gmail.com>
2023-10-31 23:05:09 -0300

    feature - implementation tests

--- tests/pages/lessons/activity/academic-paper/academic-paper.spec.tsx
+++ tests/pages/lessons/activity/academic-paper/academic-paper.spec.tsx
@@ -1,51 +1,123 @@
-// import AcademicPaper from '@/app/(private)/lessons/activity/academic-paper/[id]/page'
+import AcademicPaper from '@/app/(private)/lessons/activity/academic-paper/[id]/page'
 import { render, waitFor } from '@testing-library/react'
-// import React from 'react'
-// import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-// import { getAcademicPaperByIdMock } from '@tests/helpers'
-// import type { ReactNode } from 'react'
+import React from 'react'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { getAcademicPaperByIdMock } from '@tests/helpers'
+import type { ReactNode } from 'react'
 
-// const queryClient = new QueryClient()
-// const wrapper = ({ children }: { children: ReactNode }) => (
-//   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-// )
-// jest.mock('next/navigation', () => ({
-//   useParams: jest.fn().mockReturnValue({ id: 1 }),
-//   usePathname: jest.fn(),
-//   useRouter: jest.fn(() => ({
-//     push: jest.fn(),
-//     back: jest.fn(),
-//     forward: jest.fn(),
-//   })),
-// }))
-// describe('Academic paper', () => {
-//   beforeEach(() => {
-//     queryClient.clear()
-//   })
-//   it('Should be render a activity paper without reports', async () => {
-//     getAcademicPaperByIdMock(200)
+const queryClient = new QueryClient()
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+)
+jest.mock('next/navigation', () => ({
+  useParams: jest.fn().mockReturnValue({ id: 1 }),
+  usePathname: jest.fn(),
+  useRouter: jest.fn(() => ({
+    push: jest.fn(),
+    back: jest.fn(),
+    forward: jest.fn(),
+  })),
+}))
 
-//     const { getByText, getByRole } = render(<AcademicPaper />, {
-//       wrapper,
-//     })
-//     const loading = getByText('Carregando dados...')
+jest.mock('react-apexcharts', () => ({
+  __esModule: true,
+  default: () => <div />,
+}))
+describe('Academic paper', () => {
+  beforeEach(() => {
+    queryClient.clear()
+  })
+  it('Should be render a activity paper without reports', async () => {
+    getAcademicPaperByIdMock(200, 'PENDING')
 
-//     await waitFor(() => expect(loading).not.toBeInTheDocument())
+    const { getByText, getByRole } = render(<AcademicPaper />, {
+      wrapper,
+    })
+    const loading = getByText('Carregando dados...')
+    expect(loading).toBeVisible()
 
-//     const buttonAnalyseAcademicPaper = getByText('Analisar trabalho')
-//     const comments = getByText('Observações')
-//     const academicPaper = getByRole('academic-paper')
+    await waitFor(() => {
+      expect(loading).not.toBeInTheDocument()
+    })
 
-//     expect(buttonAnalyseAcademicPaper).toBeInTheDocument()
-//     expect(comments).toBeInTheDocument()
-//     expect(academicPaper).toBeInTheDocument()
-//   })
-// })
+    const elementTextStatus = getByText('Status')
+    const elementTextPlagiarism = getByText('Plágio')
+    const elementTextOriginality = getByText('Originalidade')
+    const buttonAnalyseAcademicPaper = getByText('Analisar trabalho')
+    const textNonFoundSource = getByText('Arquivo não revisado...')
+    const comments = getByText('Observações')
+    const academicPaper = getByRole('academic-paper')
 
-// excluir depois
-describe('test fake', () => {
-  it('test fake to pass', () => {
-    const teste = true
-    expect(teste).toBe(true)
+    expect(buttonAnalyseAcademicPaper).toBeInTheDocument()
+    expect(comments).toBeInTheDocument()
+    expect(academicPaper).toBeInTheDocument()
+    expect(elementTextStatus).toBeInTheDocument()
+    expect(elementTextPlagiarism).toBeInTheDocument()
+    expect(elementTextOriginality).toBeInTheDocument()
+    expect(textNonFoundSource).toBeVisible()
+  })
+
+  beforeEach(() => {
+    queryClient.clear()
+  })
+
+  it('Should be render a activity paper without reports', async () => {
+    getAcademicPaperByIdMock(200, 'PROCESSING')
+
+    const { getByText, queryByText, getByRole } = render(<AcademicPaper />, {
+      wrapper,
+    })
+    const loading = getByText('Carregando dados...')
+    expect(loading).toBeVisible()
+
+    await waitFor(() => {
+      expect(loading).not.toBeInTheDocument()
+    })
+
+    const elementTextStatus = getByText('Status')
+    const elementTextPlagiarism = getByText('Plágio')
+    const elementTextOriginality = getByText('Originalidade')
+    const buttonAnalyseAcademicPaper = queryByText('Analisar trabalho')
+    const textNonFoundSource = queryByText('Arquivo não revisado...')
+    const comments = getByText('Observações')
+    const academicPaper = getByRole('academic-paper')
+
+    expect(buttonAnalyseAcademicPaper).toBe(null)
+    expect(comments).toBeInTheDocument()
+    expect(academicPaper).toBeInTheDocument()
+    expect(elementTextStatus).toBeInTheDocument()
+    expect(elementTextPlagiarism).toBeInTheDocument()
+    expect(elementTextOriginality).toBeInTheDocument()
+    expect(textNonFoundSource).toBeVisible()
+  })
+
+  it('Should be render a activity paper without reports', async () => {
+    getAcademicPaperByIdMock(200, 'COMPLETED')
+
+    const { getByText, queryByText, getByRole } = render(<AcademicPaper />, {
+      wrapper,
+    })
+    const loading = getByText('Carregando dados...')
+    expect(loading).toBeVisible()
+
+    await waitFor(() => {
+      expect(loading).not.toBeInTheDocument()
+    })
+
+    const elementTextStatus = getByText('Status')
+    const elementTextPlagiarism = getByText('Plágio')
+    const elementTextOriginality = getByText('Originalidade')
+    const buttonAnalyseAcademicPaper = queryByText('Analisar trabalho')
+    const textNonFoundSource = queryByText('Arquivo não revisado...')
+    const comments = getByText('Observações')
+    const academicPaper = getByRole('academic-paper')
+
+    expect(buttonAnalyseAcademicPaper).toBe(null)
+    expect(comments).toBeInTheDocument()
+    expect(academicPaper).toBeInTheDocument()
+    expect(elementTextStatus).toBeInTheDocument()
+    expect(elementTextPlagiarism).toBeInTheDocument()
+    expect(elementTextOriginality).toBeInTheDocument()
+    expect(textNonFoundSource).toBe(null)
   })
 })
